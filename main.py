1ef4a0872cb023348c23fbebfa9202d967344f77
Elvis <834223@mystma.org>
2021-09-14 09:39:52 -0500

    TopFiveMovies

---
+++ main.py
@@ -0,0 +1,18 @@
+def topFiveMovies():
+    #print the first movie
+    print("Insidious")
+    
+    #print the second movie
+    print("Lord of the Rings")
+    
+    #print third movie
+    print("Highschool Musical")
+
+    #print fourth movie
+    print("Camp Rock")
+
+    #print fifth movie
+    print("The Presitige")
+
+#print function
+topFiveMpvies()
