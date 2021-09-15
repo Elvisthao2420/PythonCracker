aad731a2408bd50b05c97edca550d015585663c2
Elvis <834223@mystma.org>
2021-09-15 09:13:11 -0500

    TopFiveList

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#define function
 def topFiveMovies():
     #print the first movie
     print("Insidious")
@@ -14,5 +15,67 @@ def topFiveMovies():
     #print fifth movie
     print("The Presitige")
 
-#print function
-topFiveMpvies()
+def topFiveSports():
+    #print first sport
+    print("Footbal")
+
+    #print second sport
+    print("Basketball")
+
+    #print third sport
+    print("Volleyball")
+
+    #print fourth sport
+    print("Tennis")
+
+    #print fifth sport
+    print("Golf")
+
+def topFiveBears():
+    #print first bear
+    print("Pola rBear")
+
+    #print second bear
+    print("Black Bear")
+
+    #print third bear
+    print('Panda')
+
+    #print fourth bear
+    print("Red Panda")
+
+    #print fifth bear
+    print("Grizzly Bear")
+
+def topTenNumbers():
+    #print first number
+    print("1")
+
+    #print second number
+    print("2")
+
+    #print third number
+    print("64")
+
+    #print fourth number
+    print("9")
+
+    #print fifth number
+    print("99")
+
+    #print sixth number
+    print("10")
+
+    #print seventh number
+    print("12")
+
+    #print eighth number
+    print("49")
+
+    #print ninth number
+    print("69")
+
+    #print tenth number
+    print("4")
+
+topFiveMovies()
