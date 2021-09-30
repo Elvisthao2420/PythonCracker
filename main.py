b1ffe708ed96877488d44ff995ced3db4bdaf9cd
Elvis <834223@mystma.org>
2021-09-30 08:53:20 -0500

    Practice

--- main.py
+++ main.py
@@ -33,3 +33,10 @@ def define(word):
         return "Reflection of light"
 
 print(define("Word"))
+
+def function(x, y, z):
+    if x >10 or not y > 10 and z == 5:
+        return True
+    else:
+        return False
+
